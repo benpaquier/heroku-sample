ddfdaa5bd60eab2dbf1df2913ceea2ac40c0ddfc
benpaquier <hello@benpaquier.com>
2022-04-25 16:05:02 +0200

    yoyo

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ const express = require("express")
 const app = express()
 const port = 5000
 
-// on dit à notre application d'utiliser 
-// handlebars en tant que view engine
-const engine = require("express-handlebars").engine
-app.engine("handlebars", engine())
-app.set('view engine', 'handlebars')
-
-// routes
-const homeRoutes = require('./routes/home')
-app.use('/home', homeRoutes)
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
